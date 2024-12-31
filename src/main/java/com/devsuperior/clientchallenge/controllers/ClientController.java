7e233697f6dde9e3ce279a4f4f7d1de8f38d9cd4
luizffdemoraes <lffm1994@gmail.com>
2024-12-30 22:43:40 -0300

    Addition of paginated query route

--- src/main/java/com/devsuperior/clientchallenge/controllers/ClientController.java
+++ src/main/java/com/devsuperior/clientchallenge/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.clientchallenge.controllers;
 import com.devsuperior.clientchallenge.dto.ClientDTO;
 import com.devsuperior.clientchallenge.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,5 +21,10 @@ public class ClientController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) throws Exception {
         return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 }
--- src/main/java/com/devsuperior/clientchallenge/services/ClientService.java
+++ src/main/java/com/devsuperior/clientchallenge/services/ClientService.java
@@ -4,6 +4,8 @@ import com.devsuperior.clientchallenge.dto.ClientDTO;
 import com.devsuperior.clientchallenge.entities.Client;
 import com.devsuperior.clientchallenge.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,5 +19,11 @@ public class ClientService {
     public ClientDTO findById(Long id) throws Exception {
        Client client = repository.findById(id).orElseThrow(() -> new Exception("Recurso não encontrado"));
        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> clientPage = repository.findAll(pageable);
+        return clientPage.map(x -> new ClientDTO(x));
     }
 }
